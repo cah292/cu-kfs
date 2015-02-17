e0c2bc86289cfc2cd6559b63fa2008887898f5f3
db483 <db483@cornell.edu>
2015-02-17 10:54:40 +0000

    Revert "KFSPTS-3933 Add business rule to check if the award accounts are already in use on another Award."
    This reverts commit 9664b18acd5fe38b095af6b3c850b518e54dc75f.

--- src/main/java/edu/cornell/kfs/sys/CUKFSKeyConstants.java
+++ src/main/java/edu/cornell/kfs/sys/CUKFSKeyConstants.java
@@ -111,8 +111,5 @@ public class CUKFSKeyConstants extends KFSKeyConstants {
 	    // KFSPTS-3416
 	    public static final String MESSAGE_DONE_FILE_ALREADY_EXISTS = "message.system.batch.doneFileAlreadyExists";
 	    public static final String MESSAGE_DONE_FILE_SUCCESSFULLY_CREATED = "message.system.batch.doneFileSuccessfullyCreated";
-	    	    
-	    // KFSPTS-3933
-	    public static final String ERROR_AWARD_ACCOUNT_ALREADY_IN_USE = "error.award.awardAccount.alreadyInUse";
-	    
+
 }
